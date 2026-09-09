a304879316c548e37aebdf323255494ff80b3793
geeklay <13053807+geeklay@users.noreply.github.com>
2026-09-09 16:40:47 -0700

    refactor phase

--- tdd_lab/src/counter.py
+++ tdd_lab/src/counter.py
@@ -8,11 +8,14 @@ app = Flask(__name__)
 
 COUNTERS = {}
 
+def counter_exists(name):
+    """Check if counter exists"""
+    return name in COUNTERS
+
 @app.route('/counters/<name>', methods=['POST'])
 def create_counter(name):
     """Create a counter"""
-    if name in COUNTERS:
+    if counter_exists(name):
         return jsonify({"error": f"Counter {name} already exists"}), status.HTTP_409_CONFLICT
     COUNTERS[name] = 0
     return jsonify({name: COUNTERS[name]}), status.HTTP_201_CREATED
-
