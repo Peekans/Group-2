34e0b11a41d254a68adaef0527c27a6a21ad5a17
Peekans <192701619+Peekans@users.noreply.github.com>
2026-09-18 10:14:52 -0700

    Used a valid name for test

--- tdd_lab/tests/test_counter.py
+++ tdd_lab/tests/test_counter.py
@@ -111,7 +111,7 @@ class TestCounterEndpoints:
 
     def test_retrieve_nonexistent_counter(self, client):
         """It should return 404 when retrieving a counter that does not exist"""
-        result = client.get('/counters/does-not-exist')
+        result = client.get('/counters/nonexistent')
 
         assert result.status_code == status.HTTP_404_NOT_FOUND
-        assert result.get_json() == {'error' : 'Counter does-not-exist does not exist'}
+        assert result.get_json() == {'error' : 'Counter nonexistent does not exist'}
